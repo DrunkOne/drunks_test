599d71a3218cc4c0a89fb9d8f22e44498e490258
DrunkOne <122336534+DrunkOne@users.noreply.github.com>
2023-01-16 14:47:05 +0700

    Update test.c
    I made it more retarded

--- test.c
+++ test.c
@@ -1,9 +1,12 @@
 //suck my cock daniel
 #include <stdio.h>
+#define SIZE 10
 
-int selection (char input) {
+char password[SIZE];
+
+void selection_menu () {
+    char selection;
     printf("How to create a program? \n");
-
     do {
         printf("Choose one of the two options: A or B: ");
         scanf("%c", selection);
@@ -17,11 +20,32 @@ int selection (char input) {
             printf("Invalid command. \n");
         }
     } while (1);
-    return input;
 }
 
-int main () {
-    char input = selection(input);
+int assign_password () {
+    printf("Enter dick length: ");
+    scanf(" %[^\n]s", password);
+    return password;
+}
 
+void change_password () {
+    printf("Enter new password: ");
+    scanf(" %[^\n]s", password);
+}
+
+
+int main () {
+    printf("Your password is %s \n", assign_password);
+    printf("You want to change your password? (Y/N): ");
+    int input;
+    scanf("%c", input);
+    if (input == 'Y' || input == 'y') {
+        change_password(); 
+    } else if (input == 'N' || input == 'n') {
+        printf("Creating 1000 instances of Bonzi buddy...");
+    } else {
+        printf("You fucking muppet");
+        selection();
+    }
     return 0;
 }
