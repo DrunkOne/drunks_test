0eecce9076a45d9ef995fe41dfe26137798c88d3
Drunk_One <laio.daniel@gmail.com>
2023-01-16 13:13:56 +0700

    I want to die

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int selection (char input) {
+    printf("How to create a program? \n");
+
+    do {
+        printf("Choose one of the two options: A or B: ");
+        scanf("%c", selection);
+        if( selection == 'A' || selection == 'B') {
+            printf("Lmao sonnnn I don't have an idea at all on how to program anything... \n");
+            break;
+        } else if ( selection == 'B' || selection == 'b') {
+            printf("What you still doing here? \n");
+            break; 
+        } else {
+            printf("Invalid command. \n");
+        }
+    } while (1);
+    return input;
+}
+
+int main () {
+    char input = selection(input);
+
+    return 0;
+}
